2f0581ffdff4415a292159afdc5d7cf5c22e77da
Lee Dohm <lee-dohm@users.noreply.github.com>
2017-05-27 22:29:13 -0700

    Add framework for mix task

---
+++ lib/octicons/mix/tasks/build.ex
@@ -0,0 +1,9 @@
+defmodule Mix.Tasks.Octicons.Build do
+  use Mix.Task
+
+  @shortdoc "Pull down the necessary files and copy to their rightful places"
+
+  def run(_) do
+    IO.puts "Something important will go here"
+  end
+end
