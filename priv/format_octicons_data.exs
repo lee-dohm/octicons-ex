c39d44f71110cf258bbe70321400f618f433807b
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-06-04 12:50:53 -0700

    Create script to package data in Elixir script format

---
+++ priv/format_octicons_data.exs
@@ -0,0 +1,15 @@
+data_path = Path.expand("../node_modules/octicons/build/data.json", __DIR__)
+metadata_path = Path.expand("../node_modules/octicons/package.json", __DIR__)
+
+data =
+  data_path
+  |> File.read!()
+  |> Jason.decode!()
+
+metadata =
+  metadata_path
+  |> File.read!()
+  |> Jason.decode!()
+
+File.write!(Path.join(__DIR__, "data.exs"), inspect(data, limit: :infinity, pretty: true, width: 80))
+File.write!(Path.join(__DIR__, "metadata.exs"), inspect(metadata, limit: :infinity, pretty: true, width: 80))
